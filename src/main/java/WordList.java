c520d979175b3342808ba36eeca252e61fc527f3
sebastiankoster <142959691+sebastiankoster@users.noreply.github.com>
2024-04-04 11:57:54 -0700

    Update WordList.java

--- src/main/java/WordList.java
+++ src/main/java/WordList.java
@@ -12,10 +12,9 @@ public class WordList
   {
     int sum = 0;
     for(int i =0; i<myList.size(); i++)
-        if(myList.get(i).length==len)
+        if(myList.get(i).length()==len)
             sum++;
-    return sum
-  ;
+    return sum;
   }
 
   public void removeWordsOfLength(int len)
